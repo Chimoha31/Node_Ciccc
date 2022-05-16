6d98243ce01dba0eda9fe4524f9bfcecd7aeceff
Chimoha31 <mokochii1108@gmail.com>
2022-05-16 15:37:22 -0700

    Done Homework

---
+++ 03Day/app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+const PORT = 5000;
+
+let animalStudents = [
+  {
+    animalRollNumber: "1",
+    animalName: "Mr. Dog",
+    animalCollege: "Doggy university",
+    animalAge: "5",
+  },
+  {
+    animalRollNumber: "2",
+    animalName: "Mrs.Cat",
+    animalCollege: "Kitty university",
+    animalAge: "7",
+  },
+  {
+    animalRollNumber: "3",
+    animalName: "Pengine",
+    animalCollege: "Penpen university",
+    animalAge: "100",
+  },
+];
+
+app.get("/", (req, res) => {
+  return res.status(200).json(html);
+});
+
+app.get("/animals", (req, res) => {
+  return res.status(200).json(animalStudents);
+});
+
+app.get("/animals/:animalRollNumber", (req, res) => {
+  const reqId = req.params.animalRollNumber;
+  const foundAnimal = animalStudents.find((student) => {
+    if (student.animalRollNumber == reqId) {
+      return student;
+    }
+  });
+  return res.status(200).json(foundAnimal);
+});
+
+app.listen(PORT, () => {
+  console.log("Teddy is Running");
+});
