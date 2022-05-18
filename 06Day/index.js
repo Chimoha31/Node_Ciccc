605b9fbf8e5feff0b6cf8810728e675ca7141d2b
Chimoha31 <mokochii1108@gmail.com>
2022-05-18 13:10:20 -0700

    Done HW (Checked data in postman & mongodb)

--- 06Day/index.js
+++ 06Day/index.js
@@ -2,14 +2,83 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const PORT = 5000;
+const Student = require("./models/student");
 
 app.use(express.json());
 
 const mongoURL = `mongodb+srv://chiho:chiho@ciccc-may08.fhcts.mongodb.net/?retryWrites=true&w=majority`;
 
+// Connect to mongodb --------------------------------------
+mongoose.connect(mongoURL, (err) => {
+  if (err) {
+    console.log(`There was an error connecting in database ${err}`);
+  } else {
+    console.log("Successfully connected to database");
+  }
+});
+//-----------------------------------------------------------
 app.get("/", (req, res) => {
   res.send("Hello Mongodb");
 });
+
+// fetch all students list-----------------------------
+app.get("/api/v1/students", (req, res) => {
+  Student.find()
+    .then((data) => {
+      return res.status(200).json({
+        message: "Succesfully feched to data",
+        data: data,
+      });
+    })
+    .catch((err) => {
+      return res.status(500).json({
+        message: "There wa an error",
+        err: err,
+      });
+    });
+});
+
+// Fetch the student data by given Id----------------------
+app.get("/api/v1/students/:id", (req, res) => {
+  const id = req.params.id;
+  Student.findById(id)
+    .then((data) => {
+      return res.status(200).json({
+        message: "Succesfully fetched to student list",
+        data: data,
+      });
+    })
+    .catch((err) => {
+      return res.status(500).json({
+        message: "There was an error",
+        err: err,
+      });
+    });
+});
+
+// Create entry in mongodb---------------------------
+app.post("/api/v1/students", async (req, res) => {
+  const newStudent = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  };
+  const student = new Student(newStudent);
+
+  try {
+    const data = await student.save();
+    return res.status(201).json({
+        message: "Succesfully created a student!",
+        data
+    })
+} catch(error) {
+    return res.status(500).json({
+        message: "There was an error fetching the data",
+        error
+    })
+}
+
+})
 
 app.listen(PORT, () => {
   console.log(`Teddy is running at PORT ${PORT} 🐶`);
---
+++ 06Day/models/student.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const StudentSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  }
+});
+
+const Student = mongoose.model("StudentSchema", StudentSchema);
+module.exports = Student
