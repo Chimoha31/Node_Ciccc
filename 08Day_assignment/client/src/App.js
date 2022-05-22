1baae22a0c6ef6bd4e456bf08c96bb9cf6b17583
Chimoha31 <mokochii1108@gmail.com>
2022-05-21 21:41:53 -0700

    Installed React router dom & tailwind

--- 08Day_assignment/client/src/App.js
+++ 08Day_assignment/client/src/App.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { Fragment } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Author from './components/Author';
+import Book from './components/Book';
+import BookList from './components/BookList';
+
 
 const App = () => {
   return (
-    <div>
-      Hello Client Side!
-    </div>
+    <Fragment>
+      <Router>
+        <Routes>
+          <Route path="/author" element={<Author />} />
+          <Route path="/book" element={<Book />} />
+          <Route path="/booklist" element={<BookList />} />
+        </Routes>
+      </Router>
+    </Fragment>
   )
 }
 
